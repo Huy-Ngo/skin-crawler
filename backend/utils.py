3175f1dbede99b2e20aea4c17d23d5dfa9cc453c
Huy-Ngo <duchuy29092000@gmail.com>
2020-11-19 21:15:35 +0700

    Write a function to format ISIC data into convenient format

--- backend/utils.py
+++ backend/utils.py
@@ -1,12 +1,24 @@
 from kaggle import download, validate, SkinCancerMnist
+from isic import ISIC_getdata
+
+
+def get_isic_full(n_images):
+    retrieved_data = ISIC_getdata(n_images, n_images)
+    full_data = []
+    for img in retrieved_data:
+        img_full = {
+            'path': f'isic/Image/{img["Name"]}.jpg',
+            'ext_path': False,
+            'original_host': 'ISIC API',
+            'original_link': 'https://isic-archive.com/api/v1/',
+            'caption': f'ISIC {img["Caption"]}'
+        }
+        full_data.append(img_full)
+    return full_data
+
 
 
 if __name__ == '__main__':
-    if validate():
-        dataset = 'kmader/skin-cancer-mnist-ham10000'
-        dataset_path = download(dataset)
-        scm = SkinCancerMnist(dataset_path)
-        for img_metadata in scm.get_img_metadata_list():
-            print('Image path: ', img_metadata[0])
-            print('Image info: ', img_metadata[1])
-            print('===')
+    from json import dumps
+    print(dumps(get_isic_full(20), indent=2))
+
