c934042a754b509c21acd2e618cfcbb0bd3848d5
Huy-Ngo <duchuy29092000@gmail.com>
2020-11-19 21:43:52 +0700

    Update JSON structure for API server
    Each image now contains:
    
    - caption
    - original host
    - original link (URL to the page where the image is found)
    - path (URL to the image)

--- backend/utils.py
+++ backend/utils.py
@@ -1,5 +1,10 @@
-from kaggle import download, validate, SkinCancerMnist
+# from kaggle import download, validate, SkinCancerMnist
 from isic import ISIC_getdata
+from json import load
+
+with open('config.json', 'r') as f:
+    data = load(f)
+    host = data['host']
 
 
 def get_isic_full(n_images):
@@ -7,8 +12,7 @@ def get_isic_full(n_images):
     full_data = []
     for img in retrieved_data:
         img_full = {
-            'path': f'isic/Image/{img["Name"]}.jpg',
-            'ext_path': False,
+            'path': f'{host}/images/{img["Name"]}.jpg',
             'original_host': 'ISIC API',
             'original_link': 'https://isic-archive.com/api/v1/',
             'caption': f'ISIC {img["Caption"]}'
@@ -17,8 +21,6 @@ def get_isic_full(n_images):
     return full_data
 
 
-
 if __name__ == '__main__':
     from json import dumps
     print(dumps(get_isic_full(20), indent=2))
-
