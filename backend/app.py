60f4f584ec67415369b909dee3cfe442cae82b82
Huy-Ngo <duchuy29092000@gmail.com>
2020-11-19 21:46:05 +0700

    Add route to serve images and to get ISIC images

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,26 @@
 from flask import Flask
+from utils import get_isic_full
 
 app = Flask(__name__)
+
+
+@app.route('/images/<name>')
+def get_image(name):
+    """Serve images in the image folder."""
+    pass
 
 
 @app.route('/kaggle/<int:num_img>')
 def get_kaggle(num_img):
     """Get `num_img` images from Kaggle API."""
     pass
+
+
+@app.route('/isic/<int:num_img>')
+def get_isic(num_img):
+    """Get `num_img` images from ISIC API."""
+    full_data = get_isic_full(num_img)
+    return {'data': full_data}
 
 
 @app.route('/all/<int:num_img>')
