064ce05ee66b7a46857f9ef7cb3e2a7351ad3321
Huy-Ngo <duchuy29092000@gmail.com>
2020-11-19 21:46:05 +0700

    Add route to serve images and to get ISIC images

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from json import load
 from random import shuffle
 
 from flask import Flask
+from utils import get_isic_full
 
 with open('downloaded.json', 'r') as f:
     data = load(f)
@@ -9,6 +10,12 @@ with open('downloaded.json', 'r') as f:
     isic_data = data['isic']
 
 app = Flask(__name__)
+
+
+@app.route('/images/<name>')
+def get_image(name):
+    """Serve images in the image folder."""
+    pass
 
 
 @app.route('/kaggle/<int:num_img>')
@@ -29,6 +36,13 @@ def get_isic(num_img):
         shuffle(data)
         data = data[:num_img]
     return {'data': data}
+
+
+@app.route('/isic/<int:num_img>')
+def get_isic(num_img):
+    """Get `num_img` images from ISIC API."""
+    full_data = get_isic_full(num_img)
+    return {'data': full_data}
 
 
 @app.route('/all/<int:num_img>')
