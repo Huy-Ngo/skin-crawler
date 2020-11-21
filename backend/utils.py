be1571c8424c3c30064e2417f46eca611929ea07
Huy-Ngo <duchuy29092000@gmail.com>
2020-11-21 10:53:51 +0700

    Save API source for the image serving route

--- backend/utils.py
+++ backend/utils.py
@@ -12,7 +12,7 @@ def get_isic_full(n_images):
     full_data = []
     for img in retrieved_data:
         img_full = {
-            'path': f'images/{img["Name"]}.jpg',
+            'path': f'images/isic/{img["Name"]}.jpg',
             'original_host': 'ISIC API',
             'original_link': 'https://isic-archive.com/api/v1/',
             'caption': f'ISIC {img["Caption"]}'
