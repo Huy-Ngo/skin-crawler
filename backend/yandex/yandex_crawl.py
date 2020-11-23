8e08e1032bdac9a9f440e6961fd89daee80bcc4b
quanghaopro <megamanzeroandciel2000@gmail.com>
2020-11-23 23:06:55 +0700

    Update following pep8 format

--- backend/yandex/yandex_crawl.py
+++ backend/yandex/yandex_crawl.py
@@ -1,36 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
 
-def to_dict(index,sauce,img_sauce):
- table={
-     "img_link" : img_sauce,
-     "source" : sauce,
-     "host": "Yandex",
-     "tittle":"yandex"+str(index)
- }   
- return table
+
+def to_dict(index, sauce, img_sauce):
+    table = {
+         "img_link": img_sauce,
+         "source": sauce,
+         "host": "Yandex",
+         "tittle": "yandex"+str(index)
+         }
+    return table
+
 
 def spider(max_pic_num):
-    url="https://yandex.com/images/search?text=skin%20cancer%20dermoscopy"
+    url = "https://yandex.com/images/search?text=skin%20cancer%20dermoscopy"
     source_code = requests.get(url)
     plain_text = source_code.text
-    soup = BeautifulSoup(plain_text,"html.parser")
+    soup = BeautifulSoup(plain_text, "html.parser")
     counter = 1
     data_list = []
     for link in soup.findAll('a', {"class": "serp-item__link"}):
-        #get link
+        # get link
         href = link.get('href')
-        #getimage link
+        # getimage link
         string = href.split("img_url=")[1]
-        convert = string.replace("%2F","/")
-        convert2= convert.replace("%3A",":")
-        og_link=convert2.split('&text')[0]
-        
-        #get source
+        convert = string.replace("%2F", "/")
+        convert2 = convert.replace("%3A", ":")
+        og_link = convert2.split('&text')[0]
+        # get source
         newstring = string.split('%3A%2F%2F')[1]
         source = newstring.split('%2F')[0]
-
-        #adding dict
+        # adding dict
         data = {}
         data['Image source'] = source
         data['Host'] = "Yandex"
@@ -44,5 +44,6 @@ def spider(max_pic_num):
             counter = counter + 1
     return data_list
 
-if __name__ =="__main__":
+
+if __name__ == "__main__":
     spider(5)
