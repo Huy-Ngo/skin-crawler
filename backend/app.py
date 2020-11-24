e16cf5f16da21361f713b42d8c880dae852e21a6
Huy-Ngo <duchuy29092000@gmail.com>
2020-11-24 20:29:44 +0700

    Add route for Wikimedia

--- backend/app.py
+++ backend/app.py
@@ -3,12 +3,28 @@ from random import shuffle
 
 from flask import Flask
 
+from wikimedia import wikimedia_crawl
+
 with open('downloaded.json', 'r') as f:
     data = load(f)
     kaggle_data = data['kaggle']
     isic_data = data['isic']
 
 app = Flask(__name__)
+
+
+def fetch_wiki(num_img):
+    unformatted_data = wikimedia_crawl(num_img)
+    data = []
+    for entry in unformatted_data:
+        new_entry = {
+            'path': entry['Image source'],
+            'original_host': entry['Host'],
+            'original_link': entry['Original URL'],
+            'caption': entry['Title']
+        }
+        data.append(new_entry)
+    return data
 
 
 @app.route('/kaggle/<int:num_img>')
@@ -31,10 +47,17 @@ def get_isic(num_img):
     return {'data': data}
 
 
+@app.route('/wiki/<int:num_img>')
+def get_wiki(num_img):
+    """Get `num_img` images from WikiMedia."""
+    return {'data': fetch_wiki(num_img)}
+
+
 @app.route('/all/<int:num_img>')
 def get_all(num_img):
     """Get `num_img` images from all sources."""
     data = kaggle_data + isic_data
+    data += fetch_wiki(100)
     if len(data) > num_img:
         shuffle(data)
         data = data[:num_img]
