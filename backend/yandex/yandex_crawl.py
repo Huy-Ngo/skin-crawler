49c0f9696b9c83f988a0206aa1f022f7b977aece
quanghaopro <megamanzeroandciel2000@gmail.com>
2020-11-26 21:38:55 +0700

    Updating yandex_crawl.py

--- backend/yandex/yandex_crawl.py
+++ backend/yandex/yandex_crawl.py
@@ -3,9 +3,14 @@ from bs4 import BeautifulSoup
 
 
 def yandex_crawler(max_pic_num):
-    """To get images from Yandex when search for skin cancer dermoscopy.
+    """ To get images from Yandex when search for skin cancer dermoscopy.
+
+
     Parameters
+
         max_pic_num : int.
+
+
     Returns:
         A list of dictionaries.
         Each dictionary has
@@ -14,36 +19,33 @@ def yandex_crawler(max_pic_num):
             - Original URL: URL to the web page where the image is found
             - Title: The title of the image
     """
-    url = "https://yandex.com/images/search?text=skin%20cancer%20dermoscopy"
-    source_code = requests.get(url)
-    plain_text = source_code.text
-    soup = BeautifulSoup(plain_text, "html.parser")
+    url = 'https://yandex.com/images/search?text=skin%20cancer%20dermoscopy'
+    html = requests.get(url)
+    soup = BeautifulSoup(html.text, "html.parser")
     counter = 1
     data_list = []
-    for link in soup.findAll('a', {"class": "serp-item__link"}):
+    for link in soup.findAll('a', {'class': 'serp-item__link'}):
         # get link
         href = link.get('href')
-        yandex_link = "https://yandex.com/" + href
         # getimage link
-        string = href.split("img_url=")[1]
-        convert = string.replace("%2F", "/")
-        convert2 = convert.replace("%3A", ":")
-        og_link = convert2.split('&text')[0]
+        img_link = href.split("img_url=")[1]
+        img_link = img_link.replace("%2F", "/")
+        img_link = img_link.replace("%3A", ":")
+        img_link = img_link.split('&text')[0]
         # adding dict
         data = {}
-        data['Image source'] = og_link
-        data['Host'] = "Yandex"
-        data['Origin URL'] = yandex_link
-        data['Title'] = "\n".join([img['alt'] for img in link.findChildren(
-            "img", alt=True)])
+        data['image source'] = img_link
+        data['host'] = 'Searched via Yandex'
+        data['origin URL'] = 'none'
+        data['title'] = '\n'.join([img['alt'] for img in link.findChildren(
+            'img', alt=True)])
         data_list.append(data)
 
         if (counter == max_pic_num):
             break
         else:
-            counter = counter + 1
+            counter += 1
     return data_list
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     yandex_crawler(5)
