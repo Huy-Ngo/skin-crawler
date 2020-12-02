78fb116cdc59734c856202a87c0a8cf3a49c29d3
Huy-Ngo <duchuy29092000@gmail.com>
2020-12-02 11:08:20 +0700

    Revert "Modal testing"
    This reverts commit e53588299597c7a7f7b9b606158334e285825f14.

--- frontend/script.js
+++ frontend/script.js
@@ -32,7 +32,6 @@ function displayImage(image) {
 	img.src = image.image;
 	img.alt = image.title;
 	img.className = "container__img";
-	img.className = "skinImage";
 
 	span.className = "title";
 	span.innerText = image.title;
@@ -44,35 +43,6 @@ function displayImage(image) {
 	console.log('success')
 }
 
-//Create the Modal
-function displayModal(image){
-	const div = document.createElement("div");
-	const div_c = document.createElement("div");
-	const div_s = document.createElement("div");
-	const span = document.createElement("span");
-	const img = document.createElement("img");
-
-	div.id = "myModal";
-	div.className = "modal";
-
-	span.className = "close";
-	span.innerText = "X";
-
-	img.className = "modal-content";
-	img.id = "img-full";
-
-	div_c.id = "caption";
-	div_s.id = "source";
-
-	div.appendChild(span);
-	div.appendChild(img);
-	div.appendChild(div_c);
-	div.appendChild(div_s);
-
-	imageContainer.appendChild(div);
-	console.log('modal success');
-}
-
 
 fetch('http://127.0.0.1:5000/all/0', {mode: "cors"})
 	.then(response => response.json())
@@ -80,6 +50,5 @@ fetch('http://127.0.0.1:5000/all/0', {mode: "cors"})
 		console.log(data)
 		for (const image of data.data) {
 			displayImage(image);
-			displayModal(image);
 		}
 	});
